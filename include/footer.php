d46ff7377fc74a6ebb767a483a3079151884e08d
SR PROGRAMMER <uptimisedcorporationph@gmail.com>
2023-02-15 18:26:36 +0800

    upload page design

---
+++ include/footer.php
@@ -0,0 +1,31 @@
+    <?php include 'chat.php'; ?>
+
+    <!-- BEGIN: Footer-->
+    <footer class="page-footer footer footer-static footer-light navbar-border navbar-shadow">
+      <div class="footer-copyright">
+        <div class="container"><span>&copy; 2023 Uptimised Corporation All rights reserved.</span><span class="right hide-on-small-only">Design and Developed by <a href="#">Ronnel Navarro</a></span></div>
+      </div>
+    </footer>
+
+    <!-- END: Footer-->
+    <!-- BEGIN VENDOR JS-->
+    <script src="assets/js/vendors.min.js"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <script src="assets/vendors/chartjs/chart.min.js"></script>
+    <script src="assets/vendors/chartist-js/chartist.min.js"></script>
+    <script src="assets/vendors/chartist-js/chartist-plugin-tooltip.js"></script>
+    <script src="assets/vendors/chartist-js/chartist-plugin-fill-donut.min.js"></script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN THEME  JS-->
+    <script src="assets/js/plugins.min.js"></script>
+    <script src="assets/js/search.min.js"></script>
+    <script src="assets/js/custom/custom-script.min.js"></script>
+    <script src="assets/js/scripts/customizer.min.js"></script>
+    <!-- END THEME  JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="assets/js/scripts/dashboard-modern.js"></script>
+    <script src="assets/js/scripts/intro.min.js"></script>
+    <!-- END PAGE LEVEL JS-->
+  </body>
+</html>
